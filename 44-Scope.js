66551b66269ce58198bde03b7ec47c556bf13b2f
celiatj <82901057+celiatj@users.noreply.github.com>
2024-01-29 16:32:08 +0100

    Scope

--- 44-Scope.js
+++ 44-Scope.js
@@ -5,7 +5,7 @@
 // hay dos "scopes"; El scope global que está fuera de todo, por lo que
 // se puede acceder a cualquier cosa que se defina allí desde cualquier
 // otro scope, pero él no puede acceder a las declaraciones en otros scopes.
-const variableEnElScopeGlobal = 'valorEnElScopeGlobal'
+const variableEnElScopeGlobal = "valorEnElScopeGlobal";
 // Y luego está el scope del bloque de código {}, desde el scope global no se
 // puede acceder a las declaraciones dentro del bloque, pero desde dentro del
 // bloque sí que se puede acceder a las declaraciones del scope global.
@@ -22,132 +22,149 @@ const variableEnElScopeGlobal = 'valorEnElScopeGlobal'
 // pero todas esas personas, no pueden ver a las del "subCoche" xDDDD
 
 // Vamos a un bloque, despídete del scope global.
-describe('44-Scope.js', () => {
+describe("44-Scope.js", () => {
   // Ya estamos en el primer bloque de código.
-  const variableEnElScopeDelDescribe = 'valorEnElScopeDelDescribe'
+  const variableEnElScopeDelDescribe = "valorEnElScopeDelDescribe";
 
-  it('un bloque dentro de un bloque', () => {
-    const variableEnElScopeDelPrimerTest = 'valorEnElScopePrimerTest'
+  it("un bloque dentro de un bloque", () => {
+    const variableEnElScopeDelPrimerTest = "valorEnElScopePrimerTest";
     // Comprobemos que se ve desde este bloque de código.
-    expect(variableEnElScopeGlobal).toBe(RellenameCorrectamente)
-    expect(variableEnElScopeDelDescribe).toBe(RellenameCorrectamente)
-    expect(variableEnElScopeDelPrimerTest).toBe(RellenameCorrectamente)
+    expect(variableEnElScopeGlobal).toBe("valorEnElScopeGlobal");
+    expect(variableEnElScopeDelDescribe).toBe("valorEnElScopeDelDescribe");
+    expect(variableEnElScopeDelPrimerTest).toBe("valorEnElScopePrimerTest");
     // Despídete del scope del subBloque
-  })
+  });
   // ¡Hola, primer bloque!
   // Veremos que se puede usar desde aquí...
   // Por unos problemillas técnicos tengo que resolverte estas expectaciones.
-  expect(variableEnElScopeGlobal).toBe('valorEnElScopeGlobal')
-  expect(variableEnElScopeDelDescribe).toBe('valorEnElScopeDelDescribe')
+  expect(variableEnElScopeGlobal).toBe("valorEnElScopeGlobal");
+  expect(variableEnElScopeDelDescribe).toBe("valorEnElScopeDelDescribe");
   // Desde aquí no se puede acceder a la variable variableEnElScopeDelPrimerTest
   // Si la usamos da un error y se acabó todo. Ahora haz lo que quieras, creerme
   // o descomentar la línea donde se usa la variable. Pero la volveras a comentar xD
   // variableEnElScopeDelPrimerTest
 
-  it('un coche dentro de otro, ambos con las ventanas tintadas', () => {
+  it("un coche dentro de otro, ambos con las ventanas tintadas", () => {
     // Uhm... otro "subBloque"
-    const variableEnElScopeDelSegundoTest = 'valorEnElScopeSegundoTest'
+    const variableEnElScopeDelSegundoTest = "valorEnElScopeSegundoTest";
 
-    expect(variableEnElScopeGlobal).toBe(RellenameCorrectamente)
-    expect(variableEnElScopeDelDescribe).toBe(RellenameCorrectamente)
-    expect(variableEnElScopeDelSegundoTest).toBe(RellenameCorrectamente)
+    expect(variableEnElScopeGlobal).toBe("valorEnElScopeGlobal");
+    expect(variableEnElScopeDelDescribe).toBe("valorEnElScopeDelDescribe");
+    expect(variableEnElScopeDelSegundoTest).toBe("valorEnElScopeSegundoTest");
     // ¿Crees que desde un subBloque se puede acceder a otro subBloque "igual"?
     // Creo que esta expectación hace spoiler.
     expect(() => {
-      variableEnElScopeDelPrimerTest
-    }).toThrowAnError(ReferenceError)
+      variableEnElScopeDelPrimerTest;
+    }).toThrowAnError(ReferenceError);
     // Un error de referencia... será que no sabe a qué nos referimos ^^
-  })
+  });
 
-  it('el scope de las funciones', () => {
-    const funcionConSuScope = function() {
+  it("el scope de las funciones", () => {
+    const funcionConSuScope = function () {
       // Al final es como otro bloque de código más.
-      const unaVariableEnUnaFuncion = 'unValorEnUnaFuncion'
+      const unaVariableEnUnaFuncion = "unValorEnUnaFuncion";
 
-      return unaVariableEnUnaFuncion
-    }
+      return unaVariableEnUnaFuncion;
+    };
     // Bueno y dentro del bloque de función podemos usar cualquier cosa
     // de los bloques donde se encuentra y del scope global.
     // No hace falta probarlo, ¿no? :)
-    expect(funcionConSuScope()).toBe(RellenameCorrectamente)
+    expect(funcionConSuScope()).toBe("unValorEnUnaFuncion");
     expect(() => {
-      unaVariableEnUnaFuncion
-    }).toThrowAnError(ReferenceError)
-  })
+      unaVariableEnUnaFuncion;
+    }).toThrowAnError(ReferenceError);
+  });
 
-  it('scopes de la parte estática de la clase', () => {
+  it("scopes de la parte estática de la clase", () => {
     const Clase = class {
-      static atributoEnELScopeDeLaClase = 'atributoEnELScopeDeLaClase'
+      static atributoEnELScopeDeLaClase = "atributoEnELScopeDeLaClase";
       static unMetodoEnElScopeDeLaClase() {
-        return this.atributoEnELScopeDeLaClase
+        return this.atributoEnELScopeDeLaClase;
       }
       static otroMetodoEnElScopeDeLaClse() {
-        return 'otroMetodoEnElScopeDeLaClse'
+        return "otroMetodoEnElScopeDeLaClse";
       }
       static intentandoAccederAlScopeDeLaInstancia() {
-        return this.atributoEnELScopeDeLaInstancia
+        return this.atributoEnELScopeDeLaInstancia;
       }
       constructor() {
-        this.atributoEnELScopeDeLaInstancia = 'atributoEnELScopeDeLaInstancia'
+        this.atributoEnELScopeDeLaInstancia = "atributoEnELScopeDeLaInstancia";
       }
-    }
+    };
 
-    expect(Clase.atributoEnELScopeDeLaClase).toBe(RellenameCorrectamente)
-    expect(Clase.unMetodoEnElScopeDeLaClase()).toBe(RellenameCorrectamente)
-    expect(Clase.otroMetodoEnElScopeDeLaClse()).toBe(RellenameCorrectamente)
-    expect(Clase.intentandoAccederAlScopeDeLaInstancia()).toBe(RellenameCorrectamente)
-    const clase = new Clase()
-    expect(clase.atributoEnELScopeDeLaInstancia).toBe(RellenameCorrectamente)
+    expect(Clase.atributoEnELScopeDeLaClase).toBe("atributoEnELScopeDeLaClase");
+    expect(Clase.unMetodoEnElScopeDeLaClase()).toBe(
+      "atributoEnELScopeDeLaClase"
+    );
+    expect(Clase.otroMetodoEnElScopeDeLaClse()).toBe(
+      "otroMetodoEnElScopeDeLaClse"
+    );
+    expect(Clase.intentandoAccederAlScopeDeLaInstancia()).toBe(undefined);
+    const clase = new Clase();
+    expect(clase.atributoEnELScopeDeLaInstancia).toBe(
+      "atributoEnELScopeDeLaInstancia"
+    );
     // Ahora verás mejor que el this es una cosa depende si se usa desde
     // la parte estática o desde instancia
-  })
+  });
 
-  it('scopes dentro de la instance de clase', () => {
+  it("scopes dentro de la instance de clase", () => {
     const Clase = class {
-      static atributoEnELScopeDeLaClase = 'atributoEnELScopeDeLaClase'
+      static atributoEnELScopeDeLaClase = "atributoEnELScopeDeLaClase";
       constructor() {
-        this.atributoEnELScopeDeLaInstancia = 'atributoEnELScopeDeLaInstancia'
+        this.atributoEnELScopeDeLaInstancia = "atributoEnELScopeDeLaInstancia";
       }
       unMetodoEnElScopeDeLaInstancia() {
-        return this.atributoEnELScopeDeLaInstancia
+        return this.atributoEnELScopeDeLaInstancia;
       }
       otroMetodoEnElScopeDeLaInstancia() {
-        return 'otroMetodoEnElScopeDeLaInstancia'
+        return "otroMetodoEnElScopeDeLaInstancia";
       }
       intentandoAccederAlScopeDeLaClase() {
-        return this.atributoEnELScopeDeLaClase
+        return this.atributoEnELScopeDeLaClase;
       }
-    }
+    };
 
-    expect(Clase.atributoEnELScopeDeLaClase).toBe(RellenameCorrectamente)
-    const clase = new Clase()
-    expect(clase.atributoEnELScopeDeLaInstancia).toBe(RellenameCorrectamente)
-    expect(clase.unMetodoEnElScopeDeLaInstancia()).toBe(RellenameCorrectamente)
-    expect(clase.otroMetodoEnElScopeDeLaInstancia()).toBe(RellenameCorrectamente)
-    expect(clase.intentandoAccederAlScopeDeLaClase()).toBe(RellenameCorrectamente)
-  })
+    expect(Clase.atributoEnELScopeDeLaClase).toBe("atributoEnELScopeDeLaClase");
+    const clase = new Clase();
+    expect(clase.atributoEnELScopeDeLaInstancia).toBe(
+      "atributoEnELScopeDeLaInstancia"
+    );
+    expect(clase.unMetodoEnElScopeDeLaInstancia()).toBe(
+      "atributoEnELScopeDeLaInstancia"
+    );
+    expect(clase.otroMetodoEnElScopeDeLaInstancia()).toBe(
+      "otroMetodoEnElScopeDeLaInstancia"
+    );
+    expect(clase.intentandoAccederAlScopeDeLaClase()).toBe(undefined);
+  });
   // Último ejemplo con clases, prometido ^^
-  it('los métodos de las clases son un bloque de código más', () => {
-    const variableEnElScopeDelTest = 'valorEnElScopeDelTest'
+  it("los métodos de las clases son un bloque de código más", () => {
+    const variableEnElScopeDelTest = "valorEnElScopeDelTest";
 
     const Clase = class {
       intentandoAccederAlScopeGlobal() {
-        return variableEnElScopeGlobal
+        return variableEnElScopeGlobal;
       }
 
       intentandoAccederAlScopeDelDescribe() {
-        return variableEnElScopeDelDescribe
+        return variableEnElScopeDelDescribe;
       }
 
       intentandoAccederAlScopeDelTest() {
-        return variableEnElScopeDelTest
+        return variableEnElScopeDelTest;
       }
-    }
+    };
 
-    const clase = new Clase()
-    expect(clase.intentandoAccederAlScopeGlobal()).toBe(RellenameCorrectamente)
-    expect(clase.intentandoAccederAlScopeDelDescribe()).toBe(RellenameCorrectamente)
-    expect(clase.intentandoAccederAlScopeDelTest()).toBe(RellenameCorrectamente)
-  })
-})
+    const clase = new Clase();
+    expect(clase.intentandoAccederAlScopeGlobal()).toBe("valorEnElScopeGlobal");
+    expect(clase.intentandoAccederAlScopeDelDescribe()).toBe(
+      "valorEnElScopeDelDescribe"
+    );
+    expect(clase.intentandoAccederAlScopeDelTest()).toBe(
+      "valorEnElScopeDelTest"
+    );
+  });
+});
 // Intensito ;P Lo que viene será más relajado. Prometido.
+
